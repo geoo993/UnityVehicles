6651dd08cea57eb7e6d33a2fbdb5e4ed97546f00
GEORGE QUENTIN <ggeoo93@gmail.com>
2016-07-02 21:35:05 +0100

    added hovering car

---
+++ VehiclesTest/Assets/Scripts/CameraBehind.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehind : MonoBehaviour {
+
+	// The target we are following
+	public Transform target = null;
+
+	// The distance in the x-z pos to the target
+	public float distance = 10.0f;
+
+	// the height we want the camera to be above the target
+	public float height = 10.0f;
+	public float heightDamping = 2.0f;
+	public float rotationDamping = 0.6f;
+
+	void LateUpdate ()
+	{
+		FollowTarget ();
+		LookAtTarget ();
+	}
+
+	void  FollowTarget()
+	{
+
+		// Early out if we don't have a target
+		if (!target)
+			return;
+
+		// Calculate the current rotation angles
+		float wantedRotationAngle = target.eulerAngles.y;
+		float wantedHeight = target.position.y + height;
+		float currentRotationAngle = transform.eulerAngles.y;
+		float currentHeight = transform.position.y;
+
+		// Damp the rotation around the y-axis
+		currentRotationAngle = Mathf.LerpAngle (currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
+
+		// Damp the height
+		currentHeight = Mathf.Lerp (currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+
+		// Convert the angle into a rotation
+		Quaternion currentRotation = Quaternion.Euler (0, currentRotationAngle, 0);
+
+		// Set the position of the camera on the x-z plane to:
+		// distance meters behind the target
+		transform.position = target.position;
+		transform.position -= currentRotation * Vector3.forward * distance;
+
+		// Set the height of the camera
+		transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
+
+	}
+
+	void LookAtTarget(){
+
+		// Always look at the target
+		transform.LookAt (target);
+	}
+
+
+
+}
---
+++ VehiclesTest/Assets/Scripts/HoverController.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoverController : MonoBehaviour {
+
+	//values that controls the vehicle
+	public float acceleration, rotationRate;
+
+	//values for faking a nice turn display;
+	public float turnRotationAngle, turnRotationSeekSpeed;
+
+	//reference variable we don't directly use:
+	private float rotationVelocity, groundAngleVelocity;
+
+	private Rigidbody rigidB;
+
+	public GameObject[] all = null;
+
+
+	void Awake () 
+	{
+		rigidB = GetComponent<Rigidbody> ();
+		rigidB.mass = 1000f;
+		rigidB.drag = 1f;
+		rigidB.angularDrag = 1f;
+
+		for (int i = 0; i < 100; i++) {
+			
+			Vector3 pos = new Vector3(Random.Range (-200.0f, 200.0f), Random.Range (2.0f, 10.0f),Random.Range (-200.0f, 200.0f));
+			GameObject a = (GameObject) Instantiate(all [Random.Range (0, all.Length - 1)], pos, Quaternion.identity);
+
+			Vector3 scale = new Vector3 (Random.Range (1.0f, 10.0f), Random.Range (1.0f, 10.0f), Random.Range (1.0f, 10.0f));
+			a.transform.localScale = scale;
+		}
+	}
+
+
+	void FixedUpdate ()
+	{
+
+
+		//check if we are touching the ground:
+		if (Physics.Raycast (transform.position, transform.up * -1, 3f)) {
+			////we are on the ground; enable the accelartor and increase drag:
+			rigidB.drag = 1;
+
+			////calculate forward force:
+			Vector3 forwardForce = transform.forward * acceleration * Input.GetAxis ("Vertical");
+
+			////correct the force for deltatime and vehical mass:
+			forwardForce = forwardForce * Time.deltaTime * rigidB.mass;
+
+			rigidB.AddForce (forwardForce);
+
+		} else {
+			//we aren't on the ground and dont want to just halt in the mid-air; reduce drag:
+			rigidB.drag = 0;
+		}
+
+
+		////you can turn in the air or on the ground:
+		Vector3 turnTorque = Vector3.up * rotationRate * Input.GetAxis ("Horizontal");
+
+		////correct the force for deltatime and vehicle mass:
+		turnTorque =turnTorque * Time.deltaTime * rigidB.mass;
+		rigidB.AddTorque (turnTorque);
+
+
+		////"Fake" rotate the car when you are turning:
+		Vector3 newRotation = transform.eulerAngles;
+		float zRotation = Mathf.SmoothDampAngle (
+			newRotation.z, Input.GetAxis ("Horizontal") * -turnRotationAngle,
+			                  ref rotationVelocity, turnRotationSeekSpeed);
+		newRotation = new Vector3 (newRotation.x, newRotation.y, zRotation);
+		transform.eulerAngles = newRotation;
+
+
+
+
+
+
+
+	}
+}
